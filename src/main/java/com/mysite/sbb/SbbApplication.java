1b2a531a41ee0e375fc847123dc9a51a5ab2872a
yanghohu <yanghohu@gmail.com>
2023-08-27 11:56:55 +0900

    셋팅

---
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -0,0 +1,14 @@
+package com.mysite.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbApplication {
+
+	public static void main(String[] args) {
+		System.out.println("첫시작");
+		SpringApplication.run(SbbApplication.class, args);
+	}
+
+}
