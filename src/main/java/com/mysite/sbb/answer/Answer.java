649cc14486c2f31d8ad9f419f9c117179f6518fc
yangohu <yanghohu@gmail.com>
2023-09-10 13:00:50 +0900

    answer 엔티티 추가

---
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime creDateTime;
+
+    @ManyToOne
+    private Question question;
+
+
+}
