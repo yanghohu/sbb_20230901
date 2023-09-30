844160e86f1d47c7962baf082416ed5e99df6350
yangohu <yanghohu@gmail.com>
2023-09-30 11:04:32 +0900

    save로 질문 삭제

--- src/test/java/com/mysite/sbb/SbbApplicationTests7.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests7.java
@@ -9,23 +9,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-public class SbbApplicationTests6 {
+public class SbbApplicationTests7 {
 
     @Autowired
     private QuestionRepository questionRepository;
     @Test
     void testJpa() {
-        Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?","sbb에 대해서 알고 싶습니다.");
-    }
-
-    @Test
-    void testJpa6() {
+        assertEquals(2, this.questionRepository.count());
         Optional<Question> oq = this.questionRepository.findById(1);
-        Question q = oq.orElse(null);
-        q.setSubject("수정된 제목");
-        questionRepository.save(q);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
     }
 
 }
