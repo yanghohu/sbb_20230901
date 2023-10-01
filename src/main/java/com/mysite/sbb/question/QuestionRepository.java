0e67f6c115230930299fd978300e70b67fe87999
yangohu <yanghohu@gmail.com>
2023-10-01 10:36:25 +0900

    외래키 살리고 테스트케이스 통과하도록

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,6 +1,10 @@
 package com.mysite.sbb.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>  {
 
@@ -9,4 +13,19 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>  {
     Question findByContent(String content);
 
     Question findBySubjectAndContent(String subject, String content);
+
+    @Modifying
+    @Transactional
+    @Query(value= "Set FOREIGN_KEY_CHECKS=0;",nativeQuery = true)
+    void disableForeignKeyChecks();
+
+    @Modifying
+    @Transactional
+    @Query(value= "Truncate Question",nativeQuery = true)
+    void truncate();
+
+    @Modifying
+    @Transactional
+    @Query(value= "Set FOREIGN_KEY_CHECKS=1;",nativeQuery = true)
+    void enableForeignKeyChecks();
 }
