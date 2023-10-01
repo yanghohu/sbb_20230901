62a129adddfd1dcd48c26599c96495d6619e0c63
yanghohu <yangohu@gmail.com>
2023-10-01 12:57:51 +0900

    question_list 템플릿 추가

--- src/main/java/com/mysite/sbb/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/controller/QuestionController.java
@@ -1,13 +1,25 @@
 package com.mysite.sbb.controller;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class QuestionController {
 
+    // @AutoWired 필드 주입
+    @Autowired
+    private  QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    public String list() {
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList",questionList);
         return "question_list";
     }
 }
