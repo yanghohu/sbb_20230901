886455f92f6275b6c1dffad58fd60197f720101a
yanghohu <yanghohu@gmail.com>
2023-10-03 11:30:41 +0900

    AnswerController 도입

---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb.answer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnswerService {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+
+}
--- src/main/java/com/mysite/sbb/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/controller/AnswerController.java
@@ -1,9 +1,32 @@
 package com.mysite.sbb.controller;
 
+import com.mysite.sbb.answer.AnswerService;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping("/answer")
+@RequiredArgsConstructor
 @Controller
 public class AnswerController {
+    private final AnswerService answerService;
 
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = questionService.getQuestion(id);
+        // 답변 등록 시작
+
+        // 답변 등록 끝
+        return String.format("redirect:/question/detail/%s", id);
+    }
 
 }
