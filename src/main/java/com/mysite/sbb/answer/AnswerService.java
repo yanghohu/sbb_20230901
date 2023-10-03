9fc5e5bb70cc4fb41e10ad6c08b55539d5e90b70
yanghohu <yanghohu@gmail.com>
2023-10-03 11:47:28 +0900

    답변 저장 처리

--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -1,13 +1,23 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
+@RequiredArgsConstructor
 public class AnswerService {
 
-    @Autowired
-    private AnswerRepository answerRepository;
+    private final AnswerRepository answerRepository;
 
-
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreDateTime(LocalDateTime.now());
+        answer.setQuestion(question);
+        answerRepository.save(answer);
+    }
 }
--- src/main/java/com/mysite/sbb/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/controller/AnswerController.java
@@ -24,7 +24,7 @@ public class AnswerController {
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
         Question question = questionService.getQuestion(id);
         // 답변 등록 시작
-
+        answerService.create(question,content);
         // 답변 등록 끝
         return String.format("redirect:/question/detail/%s", id);
     }
